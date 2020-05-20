55f085194c46a6d2fe5a50b3fa697f567fbd644b
= <=>
2020-05-20 20:15:49 -0300

    alterando aplicação para API

--- src/main.go
+++ src/main.go
@@ -3,11 +3,22 @@ package main
 import (
 	"fmt"
 	"math"
+	"net/http"
+	"strconv"
 )
 
 func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":6800", nil)
 	
-	doSqrt(10);
+}
+
+func handler(w http.ResponseWriter, r *http.Request){
+	timesInString := r.URL.Query().Get("times")
+	
+	times, _ := strconv.Atoi(timesInString)
+	
+	doSqrt(times);
 	printMessage();
 }
 
@@ -15,6 +26,7 @@ func doSqrt(times int){
 	x := 0.0001
 	for i := 0; i <= times; i ++ {
 		x += math.Sqrt(x)
+		fmt.Printf("%v" , x)
 	}
 }
 
