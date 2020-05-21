8f34c566fdc1556fb091b43ba660b7daf51059d6
= <=>
2020-05-21 12:35:30 -0300

    adicionando testes

--- src/main.go
+++ src/main.go
@@ -10,26 +10,45 @@ import (
 func main() {
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":6800", nil)
-	
+
 }
 
-func handler(w http.ResponseWriter, r *http.Request){
-	timesInString := r.URL.Query().Get("times")
-	
-	times, _ := strconv.Atoi(timesInString)
-	
-	doSqrt(times);
-	printMessage();
+func handler(w http.ResponseWriter, r *http.Request) {
+	valueInString := r.URL.Query().Get("value")
+
+	validateQueryString(valueInString)
+
+	value, _ := strconv.ParseFloat(valueInString, 64)
+
+	doSqrt(value)
+	printMessage()
 }
 
-func doSqrt(times int){
-	x := 0.0001
-	for i := 0; i <= times; i ++ {
-		x += math.Sqrt(x)
-		fmt.Printf("%v" , x)
-	}
+// func runSqrtMultipleTimes(times int) {
+// 	x := 0.0001
+// 	for i := 0; i <= times; i++ {
+// 		x += doSqrt(x)
+// 	}
+// }
+
+func doSqrt(value float64) float64 {
+	return math.Sqrt(value)
 }
 
-func printMessage(){
+func printMessage() {
 	fmt.Printf("William Rocks!")
+}
+
+func validateQueryString(input string) []string {
+	errors := []string{}
+
+	if input == "" {
+		errors = append(errors, "The input 'value' cannot be empty")
+	}
+
+	if _, err := strconv.ParseFloat(input, 64); err != nil {
+		errors = append(errors, "The input 'value' must be a float")
+	}
+
+	return errors
 }
